aeb463601d1bb111539611ce9d9279cb89fdf620
Shree Nakil <ukrainman7@gmail.com>
2025-09-06 13:15:57 +0530

    Update api.js

--- Front_End/src/FrameWeb/Component/api.js
+++ Front_End/src/FrameWeb/Component/api.js
@@ -1,4 +1,4 @@
-// const Base_Url= 'https://frames-web-api.onrender.com';
-const Base_Url= 'http://localhost:3005/api';
+const Base_Url= 'https://frames-web-api.onrender.com';
+// const Base_Url= 'http://localhost:3005/api';
 
 export default Base_Url;
